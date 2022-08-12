87efe4497bac0b9624182c64e06a8f4fcd557f77
916_Pop_Andrei_Fabian <andrei.fabian.pop@stud.ubbcluj.ro>
2022-08-12 09:26:22 +0300

    final changes

--- bookmark_parser.sh
+++ bookmark_parser.sh
@@ -3,7 +3,9 @@
 help_function() {
     echo "Usage ./bookmark_parser.sh [arguments]"
     echo "Arguments:"
+	echo -e "-F\tuse firefox default location"
     echo -e "-f\tfirefox bookmark file location"
+	echo -e "-C\tuse chrome default location"
     echo -e "-c\tchrome bookmark file location"
     echo -e "-o\toutput file for the script (default: stdout)"
 }
@@ -19,27 +21,6 @@ print_entry() {
 	\"url\": $6
 }" 
 }
-
-if [[ "$#" -eq 0 ]]; then
-    help_function
-    exit
-fi
-
-# TODO: add default link variables
-
-firefox_link=""
-chrome_link=""
-output_file=""
-
-while getopts f:c:o: flag
-do
-	case "$flag" in
-		f) firefox_link=${OPTARG};;
-		c) chrome_link=${OPTARG};;
-		o) output_file=${OPTARG};;
-		*) echo "incorrect arguments"; help_function; exit;;
-	esac
-done
 
 chrome_parser() {
 	jq '.roots[].children[] | .date_added, .guid, .id, .name, .type, .url' $chrome_link | while read line; do
@@ -61,18 +42,49 @@ firefox_parser() {
 	LEFT OUTER JOIN moz_places 
 	ON moz_places.id = moz_bookmarks.fk 
 	WHERE moz_places.id = moz_bookmarks.fk" | while IFS='|' read -r date_added guid id title btype url; do
-		# echo "id: $id"
-		# TODO: add chrome id count to this id
 		print_entry "$date_added" "$guid" "$id" "$name" "$btype" "$url"
-		# print_entry $date_added $guid $id $title $btype $url
 	done
 }
 
+if [[ "$#" -eq 0 ]]; then
+    help_function
+    exit
+fi
+
+firefox_link=$(find ~/snap/firefox/common/.mozilla/firefox/ -type f -name "places.sqlite")
+chrome_link=$(find ~/.config/google-chrome/Default -type f -name "Bookmarks")
+output_file=""
+
+ff_flag=0
+ch_flag=0
+
+while getopts Ff:Cc:o: flag
+do
+	case "$flag" in
+		F) ff_flag=1;;
+		f) firefox_link=${OPTARG}; ff_flag=1;;
+		C) ch_flag=1;;
+		c) chrome_link=${OPTARG}; ch_flag=1;;
+		o) output_file=${OPTARG};;
+		*) echo "incorrect arguments"; help_function; exit;;
+	esac
+done
+
 if [[ -z $output_file ]]; then	
-	chrome_parser
-	firefox_parser
+	if [[ $ch_flag -ne 0 ]]; then
+		chrome_parser
+	fi
+
+	if [[ $ff_flag -ne 0 ]]; then
+		firefox_parser
+	fi
 else
-	chrome_parser > $output_file
-	firefox_parser >> $output_file
+	if [[ $ch_flag -ne 0 ]]; then
+		chrome_parser > $output_file
+	fi
+
+	if [[ $ff_flag -ne 0 ]]; then
+		firefox_parser >> $output_file
+	fi
 fi
 
